ac15cd43705cacb7be4d6bea5ad60d369beac358
Adrián Fernández <adriansen8@hotmail.es>
2016-03-15 16:02:23 +0100

    Subida trabajo

--- Proyecto/js/presentacion.js
+++ Proyecto/js/presentacion.js
@@ -6,5 +6,18 @@ $(document).ready(function(){
 	$("#boton_desaparecer").click(function(){
 		$("#paginaContainer").hide();
 	});
+
+ 	var theWindow = $(window),
+	   $bg = $("#bg"),
+	   aspectRatio = $bg.width() / $bg.height();
+	   function resizeBg() {
+	      if ((theWindow.width() / theWindow.height()) < aspectRatio)
+	      {
+	         $bg.removeClass().addClass('bgheight');
+	      } else {
+	         $bg.removeClass().addClass('bgwidth');
+	      }
+	   }
+	   theWindow.resize(resizeBg).trigger("resize");
 	
 });
